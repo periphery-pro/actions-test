b15de3d6fc19b045d8f75d5b08305a176f49dac3
Ian Leitch <ian@leitch.io>
2026-08-18 10:55:29 +0200

    Add unused declarations to demo the folded table

---
+++ Sources/ActionsTest/Extras.swift
@@ -0,0 +1,57 @@
+// Fixture: enough unused declarations to exercise the folded table.
+
+func hookMarkers1(for value: Int) -> Int {
+    value + 1
+}
+
+func hookMarkers2(for value: Int) -> Int {
+    value + 2
+}
+
+func hookMarkers3(for value: Int) -> Int {
+    value + 3
+}
+
+func hookMarkers4(for value: Int) -> Int {
+    value + 4
+}
+
+func hookMarkers5(for value: Int) -> Int {
+    value + 5
+}
+
+func hookMarkers6(for value: Int) -> Int {
+    value + 6
+}
+
+func hookMarkers7(for value: Int) -> Int {
+    value + 7
+}
+
+func hookMarkers8(for value: Int) -> Int {
+    value + 8
+}
+
+func hookMarkers9(for value: Int) -> Int {
+    value + 9
+}
+
+func hookMarkers10(for value: Int) -> Int {
+    value + 10
+}
+
+func hookMarkers11(for value: Int) -> Int {
+    value + 11
+}
+
+func hookMarkers12(for value: Int) -> Int {
+    value + 12
+}
+
+func hookMarkers13(for value: Int) -> Int {
+    value + 13
+}
+
+func hookMarkers14(for value: Int) -> Int {
+    value + 14
+}
